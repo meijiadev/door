d198b88a9a376bc237b990a95e60f62659fbe20f
meijiadev <1050013833@qq.com>
2023-03-14 11:06:55 +0800

    Initial commit

---
+++ constant.py
@@ -0,0 +1,81 @@
+m2 = [0]
+m15 = [0]
+m3 = [0]
+m14 = [0]
+m4 = [0]
+m13 = [0]
+
+
+def init():
+    global m2
+    global m15
+    global m4
+    global m13
+    m2 = [0]
+    m15 = [0]
+    m4 = [0]
+    m13 = [0]
+
+# def init():
+#     global t4
+#     global t13
+#     global m4
+#     global m13
+#     t4 = [0]
+#     t13 = [0]
+#     m4 = [0]
+#     m13 = [0]
+#
+#
+# def addT4(value):
+#     global t4
+#     t4.append(value)
+#
+#
+# def addT13(value):
+#     global t13
+#     t13.append(value)
+#
+#
+# def addM4(value):
+#     global m4
+#     m4.append(value)
+#
+#
+# def addM13(value):
+#     global m13
+#     m13.append(value)
+#
+#
+# def setZone4(t, m):
+#     global t4
+#     t4 = t
+#     global m4
+#     m4 = m
+#
+#
+# def setZone13(t, m):
+#     global t13
+#     global m13
+#     t13 = t
+#     m13 = m
+#
+#
+# def getT4():
+#     global t4
+#     return t4
+#
+#
+# def getT13():
+#     global t13
+#     return t13
+#
+#
+# def getm4():
+#     global m4
+#     return m4
+#
+#
+# def getm13():
+#     global m13
+#     return m13
---
+++ tcp_server_test.py
@@ -0,0 +1,122 @@
+# 导入套接字模块
+import os
+import socket
+# 导入线程模块
+import threading
+import constant
+import matplotlib.pyplot as plt
+
+# 创建一个折线图
+fig = plt.figure(figsize=(16, 8), dpi=100)
+# 设置中文语言
+plt.rcParams['font.sans-serif'] = ['SimHei']  # 显示中文标签
+plt.rcParams['axes.unicode_minus'] = False
+
+# 创建两个表格，211代表创建2行1列，当前在1的位置
+ax = fig.add_subplot(4, 1, 1)             #4区
+bx = fig.add_subplot(4, 1, 2)             #13
+cx = fig.add_subplot(4, 2, 1)             #3区
+dx = fig.add_subplot(4, 2, 2)             #14区
+
+
+# 定义个函数,使其专门重复处理客户的请求数据（也就是重复接受一个用户的消息并且重复回答，直到用户选择下线）
+def dispose_client_request(tcp_client_1, tcp_client_address):
+    constant.init()
+    initPlt()
+    # 5 循环接收和发送数据
+    while True:
+        recv_data = tcp_client_1.recv(4096)
+        if len(constant.m4) > 500:
+            constant.init()
+            ax.cla()
+            bx.cla()
+            cx.cla()
+            dx.cla()
+            plt.cla()
+            initPlt()
+        if recv_data:
+            msg_str = recv_data.hex()
+            msg_list = splitStringToByteList(msg_str)
+            # print("接收到的数据：",msg_list)
+            msg_len = len(msg_list)
+            for index in range(0, msg_len):
+                if msg_list[index] == 'ff':
+                    if index + 2 < msg_len - 1:
+                        if msg_list[index + 1] == '01':
+                            if (msg_list[index + 2]) == '0c':
+                                msg_data = ' '.join(msg_list[index + 5:index + 7]).replace(' ', '')
+                                str_3 = msg_list[index + 3]
+                                if len(msg_data) != 0:
+                                    data = int(msg_data, 16)
+                                    if str_3 == '04':
+                                        #  curDate=datetime.now()
+                                        # print("第四区间16进制转10进制：%s-%d", (msg_data, data))
+                                        constant.m4.append(data)
+                                        ax.plot(list(range(len(constant.m4))), constant.m4, '-r', label='4区')
+                                    if str_3 == '0d':
+                                        constant.m13.append(data)
+                                        bx.plot(list(range(len(constant.m13))), constant.m13, '-b', label='13区')
+                                    if str_3 == '03':
+                                        constant.m2.append(data)
+                                        cx.plot(list(range(len(constant.m2))), constant.m2, '-g', label='2区')
+                                    if str_3 == '0e':
+                                        print("第14区间16进制转10进制：%s-%d", (msg_data, data))
+                                        constant.m15.append(data)
+                                        dx.plot(list(range(len(constant.m15))), constant.m15, '-y', label='14区')
+
+        else:
+            print("%s 客户端下线了..." % tcp_client_address[1])
+            tcp_client_1.close()
+            break
+        plt.draw()
+        plt.pause(0.03)
+
+
+def splitStringToByteList(bytesString):
+    bytesList = []
+    for i in range(int(len(bytesString) / 2)):
+        bytesList.append(bytesString[i * 2:i * 2 + 2])
+    return bytesList
+
+
+# 给表的Y轴位置加上标签，rotation代表让文字横着展示，labelpad代表文字距表格多远了
+def initPlt():
+    ax.set_ylabel('4区', rotation=0, fontsize=16, labelpad=20)
+    bx.set_ylabel('13区', rotation=0, fontsize=16, labelpad=20)
+    cx.set_ylabel('3区', rotation=0, fontsize=16, labelpad=20)
+    dx.set_ylabel('14区', rotation=0, fontsize=16, labelpad=20)
+    ax.grid(True)
+    bx.grid(True)
+    cx.grid(True)
+    dx.grid(True)
+
+
+if __name__ == '__main__':
+    # 1 创建服务端套接字对象
+    tcp_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # 设置端口复用，使程序退出后端口马上释放
+    tcp_server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
+
+    # 2 绑定端口
+    tcp_server.bind(("", 9999))
+
+    # 3 设置监听
+    tcp_server.listen(5)
+
+    # plt.ion()
+    # 4 循环等待客户端连接请求（也就是最多可以同时有5个用户连接到服务器进行通信）
+    while True:
+        tcp_client_1, tcp_client_address = tcp_server.accept()
+        # 创建多线程对象
+        # thd = threading.Thread(target=dispose_client_request, args=(tcp_client_1, tcp_client_address))
+        # draw()
+        # 设置守护主线程  即如果主线程结束了 那子线程中也都销毁了  防止主线程无法退出
+        # thd.setDaemon(True)
+
+        # 启动子线程对象
+        # thd.start()
+        dispose_client_request(tcp_client_1, tcp_client_address)
+
+    # 7 关闭服务器套接字 （其实可以不用关闭，因为服务器一直都需要运行）
+    # tcp_server.close()
